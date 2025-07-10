b7d66fdd982de0cad1585096970a115b9d1a8c0e
mahdi hoseinpoor <mahdi20062019@outlook.com>
2025-07-10 19:07:45 +0330

    ADD: some Common Classes

---
+++ src/Libraries/EasyPay.Common/EntityBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyPay.Common
+{
+    public class EntityBase<TKey> : IEntityBase
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public required TKey Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+    }
+    public class EntityBase : EntityBase<long>
+    {
+       
+    }
+}
