eafb0e166bbf0967a01a0f46c1c715ed8181dbea
mahdi hoseinpoor <mahdi20062019@outlook.com>
2025-07-11 19:56:57 +0330

    ADD: Create DependencyInjection for add necceseory services to asp.net project

---
+++ src/Libraries/EasyPay.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,27 @@
+﻿using EasyPay.Domain.Entites.Identity;
+using EasyPay.Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class DependencyInjection
+    {
+        public static void AddInfrastructureServices(this IHostApplicationBuilder builder)
+        {
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnectionString");
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
+            builder.Services.AddIdentityCore<ApplicationUser>(
+                options => {
+            
+                })
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+        }
+    }
+}
