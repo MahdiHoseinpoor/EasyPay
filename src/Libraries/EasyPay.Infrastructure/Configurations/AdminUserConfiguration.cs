3500cb44679418fc27abad4faa5cbd8b65139ef5
mahdi hoseinpoor <mahdi20062019@outlook.com>
2025-07-11 19:57:40 +0330

    ADD: Create simple Model Configuration for identity users

---
+++ src/Libraries/EasyPay.Infrastructure/Configurations/AdminUserConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using EasyPay.Domain.Entites.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyPay.Infrastructure.Configurations
+{
+    public class AdminUserConfiguration : IEntityTypeConfiguration<AdminUser>
+    {
+        public void Configure(EntityTypeBuilder<AdminUser> builder)
+        {
+            builder.ToTable("AdminUser");
+        }
+    }
+}
---
+++ src/Libraries/EasyPay.Infrastructure/Configurations/NaturalUserConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using EasyPay.Domain.Entites.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyPay.Infrastructure.Configurations
+{
+    public class NaturalUserConfiguration : IEntityTypeConfiguration<NaturalUser>
+    {
+        public void Configure(EntityTypeBuilder<NaturalUser> builder)
+        {
+            builder.ToTable("NaturalUser");
+        }
+    }
+}
