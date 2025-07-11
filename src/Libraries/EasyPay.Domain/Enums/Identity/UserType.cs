48c043290b3d41d6f082aacc043802109ebfe9ae
mahdi hoseinpoor <mahdi20062019@outlook.com>
2025-07-11 19:27:12 +0330

    FIX: fix the code that changed with git!

--- src/Libraries/EasyPay.Domain/Enums/Identity/UserType.cs
+++ src/Libraries/EasyPay.Domain/Enums/Identity/UserType.cs
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:src/Libraries/EasyPay.Domain/Enums/Identity/UserType.cs
-﻿namespace EasyPay.Domain.Enums.Identity
-========
-﻿namespace EasyPay.Domain.Entites.Identity
->>>>>>>> 095c8f2824b67d3227eaf22e56bb6726fe696130:src/Libraries/EasyPay.Domain/Entites/Identity/UserType.cs
+namespace EasyPay.Domain.Entites.Identity
 {
     public enum UserType
     {
