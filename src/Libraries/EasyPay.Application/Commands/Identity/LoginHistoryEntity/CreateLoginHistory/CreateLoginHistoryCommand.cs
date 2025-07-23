deca44772528d3701b81d1f0b0c6e76a6bec545c
mahdi hoseinpoor <mahdi20062019@outlook.com>
2025-07-24 00:10:34 +0330

    ADD: Create LoginHistory CreateHandler

---
+++ src/Libraries/EasyPay.Application/Commands/Identity/LoginHistoryEntity/CreateLoginHistory/CreateLoginHistoryCommand.cs
@@ -0,0 +1,45 @@
+﻿using EasyPay.Domain.Entities.Identity;
+using EasyPay.Domain.Enums.Identity;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyPay.Application.Commands.Identity.LoginHistoryEntity.CreateLoginHistory
+{
+    public class CreateLoginHistoryCommand : IRequest<Result<long>>
+    {
+        public string UserId { get; set; }
+
+        public DateTime LoginTime { get; set; } = DateTime.Now;
+
+        public string IPAddress { get; set; }
+
+        public string UserAgent { get; set; }
+
+        public string DeviceType { get; set; }
+
+        public string OperatingSystem { get; set; }
+
+        public string Browser { get; set; }
+
+        public string Location { get; set; }
+
+        public LoginStatus Status { get; set; } = LoginStatus.Success;
+
+        public string FailureReason { get; set; }
+
+        public string SessionId { get; set; }
+
+        public bool IsPersistent { get; set; }
+
+        public string AuthenticationMethod { get; set; }
+
+        public bool TwoFactorUsed { get; set; }
+        public string TwoFactorType { get; set; }
+    }
+}
---
+++ src/Libraries/EasyPay.Application/Commands/Identity/LoginHistoryEntity/CreateLoginHistory/CreateLoginHistoryCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using EasyPay.Domain.Entities.Identity;
+using EasyPay.Infrastructure.Aggregates.Identity;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyPay.Application.Commands.Identity.LoginHistoryEntity.CreateLoginHistory
+{
+    public class CreateLoginHistoryCommandHandler : IRequestHandler<CreateLoginHistoryCommand, Result<long>>
+    {
+        ILoginHistoryRepository _loginHistoryRepository;
+        IMapper _mapper;
+        public CreateLoginHistoryCommandHandler(ILoginHistoryRepository loginHistoryRepository, IMapper mapper)
+        {
+            _loginHistoryRepository = loginHistoryRepository;
+            _mapper = mapper;
+        }
+        public async Task<Result<long>> Handle(CreateLoginHistoryCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var entity = _mapper.Map<LoginHistory>(request);
+                await _loginHistoryRepository.AddAsync(entity);
+                await _loginHistoryRepository.SaveChangesAsync();
+                return Result<long>.Success(entity.Id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
