8adfb6e5979a35e81a7f8264c29b9fb8bafc99b5
mahdi hoseinpoor <mahdi20062019@outlook.com>
2025-09-19 21:33:55 +0330

    FEAT: Modify UI

---
+++ src/Presentation/EasyPay.Web/Extensions/TransactionDisplayHelper.cs
@@ -0,0 +1,16 @@
+﻿using EasyPay.Shared.Enums.Report;
+
+namespace EasyPay.Web.Extensions;
+
+public static class TransactionDisplayHelper
+{
+    public static (string Icon, string Color, string AmountSign) GetDisplayDetails(this TransactionType type) => type switch
+    {
+        TransactionType.Deposit or TransactionType.TransferIn =>
+            ("bi bi-arrow-down-circle-fill", "var(--success-color)", "+"),
+        TransactionType.Withdrawal or TransactionType.TransferOut or TransactionType.BillPayment =>
+            ("bi bi-arrow-up-circle-fill", "var(--text-primary)", "-"),
+        _ =>
+            ("bi bi-receipt-cutoff", "var(--text-primary)", "-")
+    };
+}
